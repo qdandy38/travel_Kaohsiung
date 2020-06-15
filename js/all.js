f29fd8e29df8a2d06b064fb4682960035680d9be
YunHsuan Tsai <qdandy38@gmail.com>
2020-06-15 17:26:03 +0800

    update ES6

--- js/all.js
+++ js/all.js
@@ -2686,25 +2686,36 @@ function updateList(item) {
     let dataLen = item.length;
     let str = '';
     for (let i = 0; i < dataLen; i++) {
-        str += `<div class="col-md-6 mb-4">
-        <div class="card h-100">
-        <div class="card-header bg-cover d-flex align-items-end justify-content-between" style="background-position: center center; min-height: 155px; background-image: url(${item[i].Pic});">
-        <h4 class="card-title text-white mb-0">${item[i].Name}</h4>
-        <h6 class="text-white mb-0">${item[i].Zone}</h6></div>
-        <div class="card-body">
-        <div class="info">
-        <div class="d-flex align-items-center mb-2">
-        <img class="img-fluid" src="img/icons_clock.png" alt="" style="margin-right:8px;">
-        <p class="mb-0">${item[i].Opentime}</p></div>
-        <div class="d-flex align-items-center mb-2">
-        <img class="img-fluid" src="img/icons_pin.png" alt="" style="margin-right:10px;">
-        <p class="mb-0">${item[i].Address}</p></div>
-        <div class="d-flex align-items-center mb-2">
-        <img class="img-fluid" src="img/icons_phone.png" alt="" style="margin-left: 2px; margin-right:14px;">
-        <p class="mb-0">${item[i].Tel}</p></div></div>
-        <div class="tag">
-        <img src="img/icons_tag.png" alt="">
-        <span class="ml-2">${item[i].Ticket}</span></div></div></div></div>`
+        str += `
+        <div class="col-md-6 mb-4">
+            <div class="card h-100">
+                <div class="card-header bg-cover d-flex align-items-end justify-content-between" style="background-position: center center; min-height: 155px; background-image: url(${item[i].Pic});">
+                    <h4 class="card-title text-white mb-0">${item[i].Name}</h4>
+                    <h6 class="text-white mb-0">${item[i].Zone}</h6>
+                </div>
+                <div class="card-body">
+                    <div class="info">
+                        <div class="d-flex align-items-center mb-2">
+                            <img class="img-fluid" src="img/icons_clock.png" alt="" style="margin-right:8px;">
+                            <p class="mb-0">${item[i].Opentime}</p>
+                        </div>
+                        <div class="d-flex align-items-center mb-2">
+                            <img class="img-fluid" src="img/icons_pin.png" alt="" style="margin-right:10px;">
+                            <p class="mb-0">${item[i].Address}</p>
+                        </div>
+                        <div class="d-flex align-items-center mb-2">
+                            <img class="img-fluid" src="img/icons_phone.png" alt="" style="margin-left: 2px; margin-right:14px;">
+                            <p class="mb-0">${item[i].Tel}</p>
+                        </div>
+                    </div>
+                    <div class="tag">
+                        <img src="img/icons_tag.png" alt="">
+                        <span class="ml-2">${item[i].Ticket}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
     }
     list.innerHTML = str;
 }
